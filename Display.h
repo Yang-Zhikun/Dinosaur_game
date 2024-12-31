a83a53fb93651a45dc19ba9817fc2e5c82e7f179
404 not found <776560548@qq.com>
2024-12-31 17:46:31 +0800

    develop files

---
+++ Display.h
@@ -0,0 +1,15 @@
+#ifndef DISPLAY_H 
+#define DISPLAY_H
+
+class Display{
+private:
+    unsigned int width;
+    unsigned int height;
+
+public:
+    Display(unsigned int width, unsigned int height);
+
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include<windows.h>
+#include"Display.h"
+int main(){
+    Display display(128,64);
+    system("pause");
+}
